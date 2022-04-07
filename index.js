8b9fe1456cd6ba198b8ec726f28345c84381ba20
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-04-06 21:45:44 -0500

    added comments to show the URL for each HTTP method

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ const app = express();
 const songValidate = require("./middleware/song-validation");
 const songLogger = require("./middleware/song-logger");
 
-
-
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-
-
 // Endpoints
+// URL: http:/localhost:5005/api/music
 app.get("/api/music", (req, res) => {
     console.log(req.headers);
     const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
 })
 
+// URL: http:/localhost:5005/api/music/:id
 app.get("/api/music/:id", (req, res) => {
     const id = req.params.id;
     const song = repoContext.songs.findSongById(id);
     return res.send(song);
 })
 
+// URL: http:/localhost:5005/api/music
 app.post("/api/music",[songLogger,songValidate], (req,res) => {
     const newSong = req.body;
     const addedSong = repoContext.songs.createSong(newSong);
     return res.status(201).send(addedSong);
 })
 
+// URL: http:/localhost:5005/api/music/:id
 app.put("/api/music/:id", (req,res) => {
     const id = parseInt(req.params.id);
     const songPropertiesToModify = req.body;
@@ -40,6 +40,7 @@ app.put("/api/music/:id", (req,res) => {
     return  res.send(songToUpdate);
 })
 
+// URL: http:/localhost:5005/api/music/:id
 app.delete("/api/music/:id", (req,res) => {
     const id = parseInt(req.params.id);
     const deletedSong = repoContext.songs.deleteSong(id);
